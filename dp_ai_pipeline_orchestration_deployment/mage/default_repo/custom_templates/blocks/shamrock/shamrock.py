533c6102f20207ff2c843eedc83ca467b80febfe
Jarcau Cristian <95632351+JarcauCristian@users.noreply.github.com>
2025-02-20 10:10:03 +0200

    Update shamrock.py

--- dp_ai_pipeline_orchestration_deployment/mage/default_repo/custom_templates/blocks/shamrock/shamrock.py
+++ dp_ai_pipeline_orchestration_deployment/mage/default_repo/custom_templates/blocks/shamrock/shamrock.py
@@ -4,6 +4,7 @@ from websockets.sync.client import connect
 from typing import Callable, Any
 from threading import Event
 from queue import Queue
+import requests
 import asyncio
 import yaml
 import json
@@ -22,8 +23,15 @@ class CustomSource(BasePythonSource):
         self.update_queue = Queue()
         config_file = get_repo_path() + "/configs/<pipeline_name>/config.yaml"
         self.accuracies = []
+        self.past_accuracies = self.accuracies
         with open(config_file, 'r') as c:
             self.conf = yaml.safe_load(c)
+        
+        if self.conf["dataset"].get("entity_id"):
+            self.load_ngsi_ld(self.conf["dataset"].get("entity_id"))
+        
+        if self.conf["model"].get("model_uri"):
+            self.save_model_locally(self.conf["model"].get("model_uri"))
 
         self.get_node()
 
@@ -31,6 +39,58 @@ class CustomSource(BasePythonSource):
         self.epoch = 0
         self.metric_name = self.conf["model"]["metrics"][0]
         self.websocket_url = "ws:mageapi:8000/mage/ws"
+    
+    def save_model_locally(self, model_uri: str):
+        response = requests.request("GET", model_uri)
+
+        file_name = response.headers.get("content-disposition").split("=")[-1]
+        model_path = get_repo_path() + "/configs/<pipeline_name>/model_files/" + file_name
+        with open(model_path, 'wb') as fp:
+            for chunk in response.iter_content(chunk_size=8192):
+                fp.write(chunk)
+        
+        self.conf["model"]["model_path"] = model_path
+
+
+    def load_ngsi_ld(self, entity_id: str):
+        host = os.getenv("NGSI_LD_HOST")
+        link_context = os.getenv("NGSI_LD_LINK_CONTEXT")
+        tenant = os.getenv("NGSI_LD_TENANT")
+        if not host or not link_context or not entity_id:
+            raise Exception("Needed information to run the block is not provided!")
+
+        bucket = {
+            'host': host,
+            'entity_id': entity_id,
+            'link_context': link_context,
+            'time_query': f'timerel=after&timeAt=2022-01-01T00:00:00Z',
+        }
+
+        stellio_broker = connector.DataStore_NGSILD(bucket['host'])
+
+        load_data = connector.LoadData_NGSILD(
+            data_store=stellio_broker, 
+            entity_id=bucket['entity_id'],
+            context=bucket['link_context'],
+            tenant=tenant,
+        )
+
+        load_data.run(bucket)
+
+        data = bucket['temporal_data']
+        data.reset_index(inplace=True)
+
+        train = data.sample(frac=0.7, random_state=42)
+
+        test = data.drop(train.index)
+
+        train_path = get_repo_path() + "/configs/<pipeline_name>/train.csv"
+        test_path = get_repo_path() + "/configs/<pipeline_name>/test.csv"
+        train.to_csv(get_repo_path() + "/configs/<pipeline_name>/train.csv", index=False)
+        test.to_csv(get_repo_path() + "/configs/<pipeline_name>/test.csv", index=False)
+
+        self.conf["dataset"]["train"] = train_path
+        self.conf["dataset"]["test"] = test_path
 
     def send_data(self, data: Any):
         """
@@ -104,12 +164,15 @@ class CustomSource(BasePythonSource):
                                 print(f"Error: {update['data']}", "Training error occurred")
                         
                         handler(self.update_queue)
-                                                
+
                         send_to_ws = {
                             "pipeline": "<pipeline_name>",
                             "type": "json",
-                            "data": self.accuracies
+                            "data": self.accuracies,
+                            "done": len(self.past_accuracies) == len(self.accuracies)
                         }
+
+                        self.past_accuracies = self.accuracies
 
                         self.send_data(send_to_ws)
             except Exception as e:
